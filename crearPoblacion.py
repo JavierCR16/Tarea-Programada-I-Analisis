2439ae73e79aa5247fb749dbd453309e1b1b6837
mena97villalobos <mena97villalobos@gmail.com>
2017-02-21 12:36:18 -0600

    Imagenes en escala de grises

--- crearPoblacion.py
+++ crearPoblacion.py
@@ -1,19 +1,18 @@
 from PIL import Image
-from random import choice
+from random import *
 
 def createPopulation(cantidad, width, height):
     arrayPoblacion = []
     tmp = 0
-    widthTmp=0
-    heightTmp=0
     size = width, height
     while (tmp!=cantidad):
-        child = Image.new("1", size, "white")
+        child = Image.new("L", size, "white")
         for x in range(size[0]):
             for y in range(size[1]):
                 if(choice([True,False])):
                     coordinates = x,y
-                    child.putpixel(coordinates, 0)
+                    child.putpixel(coordinates, randrange(0,255,1))
         arrayPoblacion.insert(tmp, child)
         tmp+=1
+    arrayPoblacion[0].save("bn.png")
     return arrayPoblacion
